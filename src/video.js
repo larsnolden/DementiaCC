22ff48fd76da7653e0c735aabc4e3aeb2773de9e
Lars Nolden <larsnolden@web.de>
2017-03-05 05:48:28 -0500

    intergated simplewebRTC

--- src/video.js
+++ src/video.js
@@ -1,46 +1,72 @@
 import React, { Component } from 'react';
-import RTC from 'rtc';
+import ReactDOM from 'react-dom'
+import { findDOMNode } from 'react-dom';
+import SimpleWebRTC from 'simplewebrtc';
+
 
 class Video extends Component {
-  constructor() {
+  constructor () {
     super();
-    this.initiateConn = this.initiateConn.bind(this);
+
+    this.addVideo = this.addVideo.bind(this);
+    this.removeVideo = this.removeVideo.bind(this);
+    this.readyToCall = this.readyToCall.bind(this);
   }
-  
-  initiateConn() {
-    // Options
-    var rtcOpts = {
-      room: 'test-room',
-      signaller: 'https://switchboard.rtc.io'
-    };
 
-    var rtc = RTC(rtcOpts);
+  componentDidMount() {
+    this.webrtc = new SimpleWebRTC({
+      localVideoEl: ReactDOM.findDOMNode(this.refs.local),
+      remoteVideosEl: "",
+      autoRequestMedia: true
+      //url: 'https://your-production-signalserver.com/'
+    });
 
-    //get video target elements
-    var localVideo = document.getElementById('local-video');
-    var remoteVideo = document.getElementById('remote-video');
+    console.log("webrtc component mounted");
+    this.webrtc.on('videoAdded', this.addVideo);
+    this.webrtc.on('videoRemoved', this.removeVideo);
+    this.webrtc.on('readyToCall', this.readyToCall);
+  }
 
-    // Display local and remote video streams
-    localVideo.appendChild(rtc.local);
-    remoteVideo.appendChild(rtc.remote);
+  addVideo(video, peer) {
+    console.log('video added', peer);
+    //  console.log(this.refs.remotes);
+    var remotes = ReactDOM.findDOMNode(this.refs.remotes);
+    console.log(remotes);
+    if (remotes) {
+      var container = document.createElement('div');
+      container.className = 'videoContainer';
+      container.id = 'container_' + this.webrtc.getDomId(peer);
+      container.appendChild(video);
+      // suppress contextmenu
+      video.oncontextmenu = function() {
+        return false;
+      };
+      console.log(container);
+      remotes.appendChild(container);
+    }
+  }
 
+  removeVideo(video, peer) {
+    console.log('video removed ', peer);
+    var remotes = ReactDOM.findDOMNode(this.refs.remotes);
+    var el = document.getElementById(peer ? 'container_' +       this.webrtc.getDomId(peer) : 'localScreenContainer');
+    if (remotes && el) {
+      remotes.removeChild(el);
+    }
+  }
 
-    rtc.on('ready', init);
+  readyToCall() {
+   return this.webrtc.joinRoom('bongo');
   }
 
   render() {
-    this.initiateConn()
     return(
       <div>
-        <div id="local-video"></div>
-        <div id="remote-video"></div>
+        <video className = "local" id = "localVideo" ref = "local" > </video> 
+        <div className = "remotes" id = "remoteVideos" ref = "remotes" > </div> 
       </div>
     )
   }
 }
 
 export default Video
-
-function init(session) {
-  session.createDataChannel('chat');
-}
