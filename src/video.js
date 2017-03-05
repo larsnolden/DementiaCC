fd289f2e4726cfe58281f56460f2630687c12fbd
Lars Nolden <larsnolden@web.de>
2017-03-05 13:03:38 -0500

    video styling

--- src/video.js
+++ src/video.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { findDOMNode } from 'react-dom';
 import SimpleWebRTC from 'simplewebrtc';
+import './video.css'
 
 
 class Video extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
 
     this.addVideo = this.addVideo.bind(this);
     this.removeVideo = this.removeVideo.bind(this);
@@ -23,6 +24,7 @@ class Video extends Component {
     });
 
     console.log("webrtc component mounted");
+    
     this.webrtc.on('videoAdded', this.addVideo);
     this.webrtc.on('videoRemoved', this.removeVideo);
     this.webrtc.on('readyToCall', this.readyToCall);
@@ -32,10 +34,9 @@ class Video extends Component {
     console.log('video added', peer);
     //  console.log(this.refs.remotes);
     var remotes = ReactDOM.findDOMNode(this.refs.remotes);
-    console.log(remotes);
     if (remotes) {
       var container = document.createElement('div');
-      container.className = 'videoContainer';
+      container.className = 'innerVideoContainer';
       container.id = 'container_' + this.webrtc.getDomId(peer);
       container.appendChild(video);
       // suppress contextmenu
@@ -63,19 +64,32 @@ class Video extends Component {
   handleButton() {
     console.log(this.webrtc)
     this.webrtc.pauseVideo();
-
   }
 
+ 
+//style={videoStyle(this.props.bidirectional)}
   render() {
     return(
-      <div>
-        <button onClick={this.handleButton}>info</button>
-        <video className = "local" id = "localVideo" ref = "local" > </video> 
-        <video src="video.webm" controls type="video/webm">lol</video>
-        <div className = "remotes" id = "remoteVideos" ref = "remotes" > </div> 
+      <div className="videoContainer">
+        {/*<button onClick={this.handleButton}>info</button>*/}
+        <video style={localVideoStyle(this.props.bidirectional)} id="localVideo" ref="local" > </video>
+        <div className="remoteVideo" id="remoteVideos" ref="remotes"> </div> 
       </div>
     )
   }
 }
 
 export default Video
+
+function localVideoStyle(bidirectional) {
+    if (bidirectional) return {
+      position: 'absolute',
+      height: '18%', 
+      width: 'auto',
+      top: 0,
+      right: 0
+    }
+    else return {
+      display: 'none'
+    }
+  }
